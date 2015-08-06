fdf1d15d627d731173a5b3466733c13173751396
newswim <ofenixculpa@gmail.com>
2015-08-06 08:53:40 -0500

    meteorpeadia examples

--- client/index.js
+++ client/index.js
@@ -2,17 +2,61 @@ Meteor.subscribe("categories");
 
 'use strict';
 
-var TreeViewDemo = BlazeComponent.extendComponent(
-    function TreeViewDemo(args) {
+var mwCategories = BlazeComponent.extendComponent(
+    function mwCategories(args) {
         // Constructor Here
     },{
         events: function() {
             return [{
-                // 'click': this.onClicked,
+              'click span.catOpen': function(event) {
+                  var name = this.name.substr(9);
+                  var open = Session.get('categoryChildren');
+                  var idx = open.indexOf(name);
+                  if (idx == -1)
+                      open.push(name);
+                  else
+                      open.splice(idx, 1);
+                  Session.set('categoryChildren', open);
+               }
             }];
-        },
-        getCategories: function(){
-          return Categories.find();
         }
     }
 ).register('TreeViewDemo');
+
+// Single use function for the current page
+Template.mwCategories.categoriesExist = function() {
+    return WikiPages.find({categories: this.name.substr(9),
+        namespace: 'category'}).fetch().length;
+}
+Template.mwCategories.pagesExist = function() {
+    var name = this.name.substr(9);
+    return WikiPages.find({categories: name, namespace: {$exists: false}}).fetch().length;
+}
+
+Template.mwCategories_pages.pages = function() {
+    var name = this.name.substr(9);
+    return WikiPages.find({categories: name, namespace: {$exists: false}},
+        {fields: {name: 1, categories: 1}, sort: { name: 1}});
+}
+
+Template.mwCategories_cats.categories = function() {
+    var name = this.name.substr(9);
+    return WikiPages.find({categories: name, namespace: 'category'},
+        {fields: {name: 1, categories: 1}, sort: { name: 1}});
+}
+
+Template.mwCategories_cats.hasChildren = function() {
+    var name = this.name.substr(9);
+    return WikiPages.find({categories: name},
+        {fields: {name: 1, categories: 1}, sort: { name: 1}}).fetch().length;
+}
+
+Template.mwCategories_cats.isReady = function() {
+    return categoryChildrenSub.ready();
+}
+Template.mwCategories_cats.isOpen = function() {
+    var name = this.name.substr(9);
+    return isolateValue(function () {
+        return _.contains(Session.get('categoryChildren'), name);
+    });
+}
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,8 @@
 Meteor.publish("categories", function () {
     return Categories.find({});
 });
+
+Meteor.publish('categoryChildren', function(categories) {
+    return WikiPages.find({categories: {$in: categories}},
+        {fields: {name: 1, categories: 1, namespace: 1}});
+});
